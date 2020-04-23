1201873bc5a7a38838c40bfaec3e50d674a9b3e5
iamshubhankarkhare <iamshubhankarkhare@gmail.com>
2020-04-23 23:27:39 +0530

    added apis -working

---
+++ src/components/Cards/Cards.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Cards(props) {
+    console.log("cards")
+    console.log(props);
+    return (
+        <div>
+            <h1>Cards</h1>
+        </div>
+    )
+}
---
+++ src/components/Chart/Chart.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Chart() {
+    return (
+        <div>
+            <h1>Charts</h1>
+        </div>
+    )
+}
