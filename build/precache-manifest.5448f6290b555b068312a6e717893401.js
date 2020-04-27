7ee31ffcfba54c4e3016d173f6d46845e4598c5c
iamshubhankarkhare <iamshubhankarkhare@gmail.com>
2020-04-27 20:24:59 +0530

    initial deploy

---
+++ build/precache-manifest.5448f6290b555b068312a6e717893401.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcb2b5fce6c98fc051d195dbaec00305",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8fdb63ded8f108b17609",
+    "url": "/static/css/2.3ab4c7df.chunk.css"
+  },
+  {
+    "revision": "4c2f44e2119edbd682d3",
+    "url": "/static/css/main.ab9424a0.chunk.css"
+  },
+  {
+    "revision": "8fdb63ded8f108b17609",
+    "url": "/static/js/2.634a21c9.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/static/js/2.634a21c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c2f44e2119edbd682d3",
+    "url": "/static/js/main.c63de70c.chunk.js"
+  },
+  {
+    "revision": "bafcac8dc50062407e05",
+    "url": "/static/js/runtime-main.22cc0548.js"
+  },
+  {
+    "revision": "ceebfe2f3492fd4e66ddc4b5d7cf630d",
+    "url": "/static/media/covid3.ceebfe2f.png"
+  },
+  {
+    "revision": "2308a9bbc10d2b931557ebe03628fcee",
+    "url": "/static/media/empty.2308a9bb.svg"
+  },
+  {
+    "revision": "7b0b72be9e5cca2d76375ad8bfb07cf4",
+    "url": "/static/media/hand_wash.7b0b72be.svg"
+  },
+  {
+    "revision": "66c309eaa121b1de6525ca673f4484cf",
+    "url": "/static/media/medicare.66c309ea.svg"
+  },
+  {
+    "revision": "138cdec259f02d2e046951d183b02f44",
+    "url": "/static/media/social_distance.138cdec2.svg"
+  }
+]);
